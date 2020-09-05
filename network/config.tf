faa03fb6f2d54d744ad28f8475c94a1078722cd3
hiromi fukuda <63894985+hiromi-dev@users.noreply.github.com>
2020-09-05 17:36:42 +0900

    .terraform-versionの追加

--- network/config.tf
+++ network/config.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-tfstate-secure-bucket-hiromi"
     key     = "netowork/terraform.tfstate"
-    profile = "mad-playground"
+    region  = "ap-northeast-1"
     encrypt = true
     profile = "mad-playground"
   }
